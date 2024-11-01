14e41df746eb67f2afa630177317fbb80c3acfe3
Mohammad Motaghianfar <motaghianfar@gmail.com>
2024-11-01 18:32:17 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
-import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
@@ -12,13 +10,18 @@ model = joblib.load('price_prediction_model.pkl')
 
 @app.route('/')
 def home():
-    return "Welcome to the Price Prediction API!"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Parse the input data
-    data = request.get_json(force=True)
-    input_data = np.array(data['input']).reshape(1, -1)
+    input_data = [
+        float(request.form['avg_area_income']),
+        float(request.form['avg_area_house_age']),
+        float(request.form['avg_area_num_rooms']),
+        float(request.form['avg_area_num_bedrooms']),
+        float(request.form['area_population'])
+    ]
+    input_data = np.array(input_data).reshape(1, -1)
 
     # Scale the input data using the same scaler used for training
     scaler = StandardScaler()
@@ -26,7 +29,7 @@ def predict():
 
     # Make prediction
     prediction = model.predict(input_data)
-    return jsonify({'prediction': prediction.tolist()})
+    return render_template('index.html', prediction=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
